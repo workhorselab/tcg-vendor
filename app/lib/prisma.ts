475e9df174f15856a40c36cb1305274dc7636479
wonjae <workhorse.lab@gmail.com>
2026-02-23 00:00:19 -0500

    chore: set up prisma

---
+++ app/lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
